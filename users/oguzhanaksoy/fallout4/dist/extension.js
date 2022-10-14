3e5f2d7e2d37ed57d2946b49a693db18a849e482
Oguzhan Aksoy <aksoy.ouzhan@nextup.ro>
2022-10-14 17:14:26 +0300

    fallout4 theme added

---
+++ users/oguzhanaksoy/fallout4/dist/extension.js
@@ -0,0 +1,1 @@
+(function(a){"use strict";let t;var e={load(){t=a.injectCSS('@import"https://commandcrafterhd.github.io/Fallout4TerminalTheme/css/main.css";')},unload(){t()}};return e})(acord.patcher);
